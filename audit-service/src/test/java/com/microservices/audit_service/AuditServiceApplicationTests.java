67df7dcd7ce121b514426c87ab9d728f2bf09fc6
david <david@devoruro.com>
2025-06-24 02:42:56 +0000

    add microservices configuration

---
+++ audit-service/src/test/java/com/microservices/audit_service/AuditServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservices.audit_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuditServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
